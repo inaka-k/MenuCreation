ecf0de6c22923f0fdb9e54cbff5a1fa49a125acb
inaka-k <inaka.k.system@gmail.com>
2020-06-28 18:30:50 +0900

    initial commit

---
+++ src/main/java/com/inak/menucreation/MenuCreationApplication.java
@@ -0,0 +1,13 @@
+package com.inak.menucreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuCreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuCreationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inak/menucreation/MenuCreationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inak.menucreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuCreationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
